1fc638e90f2e2dc84454c58ef11d936629b7c1e2
Fjborrero178 <fjborrero@uninorte.edu.co>
2022-11-15 11:21:04 -0500

    first commit

--- app.js
+++ app.js
@@ -74,13 +74,9 @@ app.get("/estilo.css", (req, res) => {
 //app.use('/Datos', require('./route/Datos.js'));
 
   
-
-app.listen(3000, () => {
-  console.log("server listening on port",3000);
+app.listen(process.env.PORT, () => {
+  console.log("server listening on port", process.env.PORT||3000);
 });
-//app.listen(process.env.PORT, () => {
-  //console.log("server listening on port", process.env.PORT||3000);
-//});
 //MIDDLEWARE
 
 //DATA BASE
