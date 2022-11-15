d16fa847a9130c1081f9821108566c96099258c8
Fjborrero178 <fjborrero@uninorte.edu.co>
2022-11-14 21:52:11 -0500

    first commit

--- app.js
+++ app.js
@@ -3,15 +3,48 @@ const path = require("path");
 const mongoose = require('mongoose');
 //const Datos = require("./models/datos.js");
 const config = require('dotenv').config();
-
+const router = express.Router();
 //express.use(express.json());
 //express.use(express.text());
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname + "/index.html"));
+const datosSchema = new mongoose.Schema({
+  Modo: {
+    type: String,
+    required: true,
+  },
+  Bateria: {
+    type: String,
+    required: true,
+  },
+  Mayor: {
+    type: String,
+    required: true,
+  },
 });
+
+const datos = mongoose.model("datos", datosSchema);
+
+app.get("/", async(req, res) => {
+  res.sendFile(path.join(__dirname + "/index.html"));
+});
+
+app.get('/envio', (req, res) => {
+  const vamos = {
+    Modo: "siuuuu", // Modo
+    Bateria: "234", // Modo
+    Mayor: "aveces" // Modo
+  }
+    
+    var Proceso = new datos (vamos);
+    //res.json([Proceso.Bateria])
+    //await Proceso.save();
+    console.log(Proceso.Bateria)
+  res.json(Proceso.Bateria);
+//{data : 'example'}
+})
+
 
 app.get("/historicos", (req, res) => {
     res.sendFile(path.join(__dirname + "/historicos.html"));
@@ -21,12 +54,12 @@ app.get("/estilo.css", (req, res) => {
     res.sendFile(path.join(__dirname + "/estilo.css"));
 });
 
-app.use('/Datos', require('./route/Datos.js'));
+//app.use('/Datos', require('./route/Datos.js'));
 
   
 
-app.listen(3000, () => {
-  console.log("server listening on port", 3000);
+app.listen(process.env.PORT, () => {
+  console.log("server listening on port", process.env.PORT||3000);
 });
 //MIDDLEWARE
 
--- route/Datos.js
+++ route/Datos.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-const Datos = require('../models/datos')
+const Datos = require('../models/datos.js')
 
 router.get('/', async (req, res) => {
     try {
         const arrayDatos = await Datos.find();
         console.log(arrayDatos)
+        
     } catch (error) {
         console.log(error)
     }
